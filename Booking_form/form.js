283976c58c3ea12116a18a09fd0ae82acbbc85b4
nagiarjun05 <nagiarjun05@gmail.com>
2022-10-20 01:04:42 +0530

    EditAndDeleteButton

--- Booking_form/form.js
+++ Booking_form/form.js
@@ -10,29 +10,49 @@ var show=document.getElementById('shw');
 show.addEventListener('click', showDetails);
 submit.addEventListener('click', onClick);
 userList.addEventListener('click', deleteUser);
-
+userList.addEventListener('click', editDetails);
 
 function onClick(e){
-    e.preventDefault();
-    var li = document.createElement('li')
-    li.className='user'
-    // li.appendChild(document.createTextNode(userName.value+'  :  '+userEmail.value+'   '));
-    li.appendChild(document.createTextNode(userName.value));
-    li.appendChild(document.createTextNode(' : '));
-    li.appendChild(document.createTextNode(userEmail.value));
-    li.appendChild(document.createTextNode('   '));
-    var deleteButton=document.createElement('button');
-    deleteButton.className='dltbtn';
-    deleteButton.appendChild(document.createTextNode('X'));
-    li.appendChild(deleteButton);
+    if (userEmail.value!=''||userName.value!=''){
+        e.preventDefault();
+        var li = document.createElement('li')
+        li.className='user'
+        // li.appendChild(document.createTextNode(userName.value+'  :  '+userEmail.value+'   '));
+        li.appendChild(document.createTextNode(userName.value));
+        li.appendChild(document.createTextNode('  '));
+        li.appendChild(document.createTextNode(userEmail.value));
+        li.appendChild(document.createTextNode('   '));
+        
+        var deleteButton=document.createElement('button');
+        deleteButton.className='dltbtn';
+        deleteButton.style.border='solid 3px red';
+        deleteButton.appendChild(document.createTextNode('Delete'));
+        li.appendChild(deleteButton);
 
-    let user= {
-        emailId:userEmail.value,
-        name:userName.value
+        var editButton=document.createElement('button');
+        editButton.className='editbtn';
+        editButton.style.border='solid 3px green';
+        editButton.appendChild(document.createTextNode('Edit'));
+        li.appendChild(editButton);
+
+
+        let user= {
+            emailId:userEmail.value,
+            name:userName.value
+        }
+
+        localStorage.setItem(`user${userEmail.value}`,JSON.stringify(user))
+        userList.appendChild(li);
     }
+}
 
-    localStorage.setItem(`user${userEmail.value}`,JSON.stringify(user))
-    userList.appendChild(li);
+function editDetails(e){
+    e.preventDefault();
+    if (e.target.classList.contains('editbtn')){
+        userEmail.value=e.target.parentElement.childNodes[0].data
+        userName.value=e.target.parentElement.childNodes[1].data
+        userList.removeChild(e.target.parentElement)
+    }
 }
 
 
@@ -46,6 +66,8 @@ function deleteUser(e){
     }
 }
 
+
+
 function showDetails(e){
     e.preventDefault();
     Object.values(localStorage).forEach((value) => {
@@ -56,10 +78,19 @@ function showDetails(e){
         Object.values(fetchedDetails).forEach((value) => {
             li.appendChild(document.createTextNode(value));
         })
+        
         var deleteButton=document.createElement('button');
         deleteButton.className='dltbtn';
-        deleteButton.appendChild(document.createTextNode('X'));
+        deleteButton.style.border='solid 3px red';
+        deleteButton.appendChild(document.createTextNode('Delete'));
         li.appendChild(deleteButton);
-        userList.appendChild(li);  
+         
+        var editButton=document.createElement('button');
+        editButton.className='editbtn';
+        editButton.style.border='solid 3px green';
+        editButton.appendChild(document.createTextNode('Edit'));
+        li.appendChild(editButton);
+
+        userList.appendChild(li);
     })
-}
+}   
