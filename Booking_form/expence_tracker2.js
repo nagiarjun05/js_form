10040c65a26a9d335a3ce70c7575a6b134a3de28
nagiarjun05 <nagiarjun05@gmail.com>
2022-11-08 16:46:37 +0530

    CrudExpenseTracker

--- Booking_form/expence_tracker2.js
+++ Booking_form/expence_tracker2.js
@@ -1,7 +1,9 @@
+// const { default: axios } = require("axios");
+
 var filled=document.getElementById("submit");
 var eAmount=document.getElementById("amount");
 var eDisc=document.getElementById("expense-type");
-var eCat=document.getElementById("expense-cat");
+var eCat=document.getElementById("expense-catogary");
 var expList=document.getElementById("exp-list");
 
 filled.addEventListener('click',onAdd);
@@ -10,62 +12,111 @@ expList.addEventListener('click', removeExpense);
 expList.addEventListener('click', editExpDetails);
     
 
-for(let i=0;i<localStorage.length;i++){
-    console.log(localStorage.getItem(localStorage.key(i)));
-    var li = document.createElement('li');
-    li.className='expenseDet';
-    li.innerHTML=localStorage.getItem(localStorage.key(i));
-    var deleteExpense=document.createElement('button');
-    deleteExpense.className='dlt';
-    deleteExpense.textContent='Delete Expense';
-    deleteExpense.style.border='solid 2px red';
+var flag=true;
+
+axios({
+    method:'get',
+    url:"https://crudcrud.com/api/b7deaa66ad714b22afdf8b2272fa9917/expensesData",
+}).then(res=>{
+    res.data.forEach(element => {
+        var li = document.createElement('li');
+        li.className='expenseDet';
+        li.innerHTML=`${element.amount} - ${element.discr} - ${element.catg}`;
+        var deleteExpense=document.createElement('button');
+        deleteExpense.className='dlt';
+        deleteExpense.textContent='Delete Expense';
+        deleteExpense.style.border='solid 2px red';
 
 
-    var editExpense=document.createElement('button');
-    editExpense.className='edt';
-    editExpense.textContent='Edit Expense';
-    editExpense.style.border='solid 2px green';
-    li.appendChild(deleteExpense);
-    li.appendChild(editExpense);
-    expList.appendChild(li);
-}
-
-
+        var editExpense=document.createElement('button');
+        editExpense.className='edt';
+        editExpense.textContent='Edit Expense';
+        editExpense.style.border='solid 2px green';
+        li.appendChild(deleteExpense);
+        li.appendChild(editExpense);
+        li.id=`${element._id}`;
+        expList.appendChild(li);
+    });
+}).catch(err=>console.log(err));
 
 
 
 function onAdd(e){
     e.preventDefault();
-    var li = document.createElement('li');
-    li.className='expenseDet';
-    li.innerHTML=`${eAmount.value} ${eDisc.value} ${eCat.value} `; 
+    if(flag==true){
+        axios({
+            method:'post',
+            url:"https://crudcrud.com/api/b7deaa66ad714b22afdf8b2272fa9917/expensesData",
+            data:{
+                "amount":`${eAmount.value} `,
+                "discr":`${eDisc.value} `,
+                "catg":`${eCat.value} `
+            }
+        }).then(res=>console.log(res)).catch(err=>console.log(err));
+        
+        var li = document.createElement('li');
+        li.className='expenseDet';
+        li.innerHTML=`${eAmount.value} - ${eDisc.value} - ${eCat.value} `; 
+        
+        
+        var deleteExpense=document.createElement('button');
+        deleteExpense.className='dlt';
+        deleteExpense.textContent='Delete Expense';
+        deleteExpense.style.border='solid 2px red';
     
     
-    var deleteExpense=document.createElement('button');
-    deleteExpense.className='dlt';
-    deleteExpense.textContent='Delete Expense';
-    deleteExpense.style.border='solid 2px red';
-
-
-    var editExpense=document.createElement('button');
-    editExpense.className='edt';
-    editExpense.textContent='Edit Expense';
-    editExpense.style.border='solid 2px green';
-    li.appendChild(deleteExpense);
-    li.appendChild(editExpense);
-
-    localStorage.setItem(eDisc.value,`${eAmount.value} ${eDisc.value} ${eCat.value} `);
-
-    expList.appendChild(li);
+        var editExpense=document.createElement('button');
+        editExpense.className='edt';
+        editExpense.textContent='Edit Expense';
+        editExpense.style.border='solid 2px green';
+        li.appendChild(deleteExpense);
+        li.appendChild(editExpense);
+        expList.appendChild(li);
+    }else{
+        axios({
+            method:'post',
+            url:`https://crudcrud.com/api/b7deaa66ad714b22afdf8b2272fa9917/expensesData/${filled.name}`,
+            data:{
+                "amount":`${eAmount.value} `,
+                "discr":`${eDisc.value} `,
+                "catg":`${eCat.value} `
+            }
+        }).then(res=>console.log(res)).catch(err=>console.log(err));
+        
+        var li = document.createElement('li');
+        li.className='expenseDet';
+        li.innerHTML=`${eAmount.value} - ${eDisc.value} - ${eCat.value} `; 
+        
+        
+        var deleteExpense=document.createElement('button');
+        deleteExpense.className='dlt';
+        deleteExpense.textContent='Delete Expense';
+        deleteExpense.style.border='solid 2px red';
+    
+    
+        var editExpense=document.createElement('button');
+        editExpense.className='edt';
+        editExpense.textContent='Edit Expense';
+        editExpense.style.border='solid 2px green';
+        li.appendChild(deleteExpense);
+        li.appendChild(editExpense);
+        li.id=filled.name;
+        filled.removeAttribute("name");
+        expList.appendChild(li);
+        flag=true;
+    }
+    
 }
 
 
 function removeExpense(e){
     e.preventDefault();
     if(e.target.classList.contains('dlt')){
-        var li = e.target.parentElement
-        localStorage.removeItem(li.innerHTML.split(' ')[1]);
-        expList.removeChild(li);
+        axios({
+            method:'delete',
+            url:`https://crudcrud.com/api/b7deaa66ad714b22afdf8b2272fa9917/expensesData/${e.target.parentElement.id}`,  
+        }).then(res=>console.log(res)).catch(err=>console.log(err))
+        expList.removeChild(e.target.parentElement)
     }
 }
 
@@ -73,11 +124,14 @@ function removeExpense(e){
 function editExpDetails(e){
     e.preventDefault();
     if(e.target.classList.contains('edt')){
-        var li = e.target.parentElement
+        
+        if(flagvar li = e.target.parentElement
         eAmount.value=li.innerHTML.split(' ')[0]
-        eDisc.value=li.innerHTML.split(' ')[1]
-        eCat.value=li.innerHTML.split(' ')[2]
-        localStorage.removeItem(li.innerHTML.split(' ')[1]); 
+        eDisc.value=li.innerHTML.split(' ')[3]
+        eCat.value=li.innerHTML.split(' ')[6]
+        filled.name=li.id;
+        // console.log(filled.name);
         expList.removeChild(li);
+        flag=false;
     }
 }
